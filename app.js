cf2b664a561a1875b55453965a5994f6f4c69ec9
dvieras <david.emrich@outlook.com>
2024-02-10 22:12:11 -0300

    adicionado novas funcionalidades como: verifica chute, contando tentativas e botão reiniciar

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 let numeroSecreto = geraraNumeroSecreto();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+function exibirMensagemInicical() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicical();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    
+
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('h1', 'acertou!');
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
-function geraraNumeroSecreto () {
+function geraraNumeroSecreto() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = ' ';
+}
 
-
-
-
+function reiniciarJogo() {
+    numeroSecreto = geraraNumeroSecreto();
+    limparCampo();
+    tentativas = 1;
+    verificarChute();
+    exibirMensagemInicical();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
